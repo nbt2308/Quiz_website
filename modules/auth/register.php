23d67b7a9462138327d57b3696343f5a5ea42ef6
Hoa Du <truchoa2019@gmail.com>
2025-10-30 12:01:48 +0700

    update name

--- modules/auth/register.php
+++ modules/auth/register.php
@@ -24,7 +24,7 @@
         <div class="register-container">
             <div class="register-content">
                 <div class="register-title mb-3 mt-3">
-                    <span>Create a new account</span>
+                    <span>CREATE A NEW ACCOUNT</span>
                 </div>
                 <div class="form-register mt-3">
                     <form action="" method="post">
