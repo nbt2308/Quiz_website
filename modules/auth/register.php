a980d039649344bdfbae302dc2522363ad7b9216
nbt <nbtrong23081997@gmail.com>
2025-11-01 14:00:51 +0700

    add footer

--- modules/auth/register.php
+++ modules/auth/register.php
@@ -51,23 +51,6 @@
             </div>
         </div>
     </div>
-    <div
-        class="d-flex flex-column flex-md-row text-center text-md-start justify-content-between py-4 px-4 px-xl-5 bg-primary">
-        <!-- Copyright -->
-        <div class="text-white mb-3 mb-md-0">
-            Copyright © 2025. All rights reserved.
-        </div>
-        <!-- Copyright -->
-
-        <!-- Right -->
-        <div class="text-white mb-3 mb-md-0">
-            TH News Website
-        </div>
-        <!-- Right -->
-    </div>
-
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
-
-</html>
+    <?php
+    layoutUser("footer");
+    ?>
