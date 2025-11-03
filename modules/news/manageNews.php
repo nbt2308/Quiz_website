cb8e5bbad29b500e965664507356a25699565688
nbt <nbtrong23081997@gmail.com>
2025-11-04 00:59:53 +0700

    change icon color

--- modules/news/manageNews.php
+++ modules/news/manageNews.php
@@ -14,7 +14,7 @@ if (!$Login) {
         <div class="manageNews-content">
             <div class="manageNews-button my-3 mx-3">
                 <a href="?module=news&action=addNews" class="btn btn-primary add-button">
-                    <img class="add-icon" src="/News_website/templates/assets/images/add_circle_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt=""> Add News
+                    <img class="add-icon" src="/News_website/templates/assets/images/add_circle_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt=""> Add News
                 </a>
             </div>
             <div class="search mb-3 mx-3">
@@ -58,15 +58,15 @@ if (!$Login) {
 
                                     echo '<td >
                                             <a href="?module=news&action=editNews&id=' . $row['news_id'] . '" class="btn btn-warning btn-sm">
-                                                <img src="/News_website/templates/assets/images/edit_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="Edit">
+                                                <img src="/News_website/templates/assets/images/edit_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Edit">
                                             </a>
 
                                             <a href="?module=news&action=deleteNews&id=' . $row['news_id'] . '" class="btn btn-danger btn-sm">
-                                                <img src="/News_website/templates/assets/images/delete_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg" alt="Delete">
+                                                <img src="/News_website/templates/assets/images/delete_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="Delete">
                                             </a>
 
                                             <a href="?module=news&action=viewNews&id=' . $row['news_id'] . '" class="btn btn-info btn-sm">
-                                                <img src="/News_website/templates/assets/images/visibility_24dp_000000_FILL0_wght400_GRAD0_opsz24.svg" alt="View">
+                                                <img src="/News_website/templates/assets/images/visibility_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.svg" alt="View">
                                             </a>
 
                                         </td>';
