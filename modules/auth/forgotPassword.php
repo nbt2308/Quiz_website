09ae220bfc26dbb9fedb9ea6b7a57403e11a48b8
nbt <nbtrong23081997@gmail.com>
2025-11-04 17:13:13 +0700

    finish forgot password function

--- modules/auth/forgotPassword.php
+++ modules/auth/forgotPassword.php
@@ -115,7 +115,7 @@ if (isMethodPost()) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link rel="stylesheet" href="/News_website/templates/assets/css/auth/forgotPassword.css">
-    <title>Sign up</title>
+    <title>Forgot password</title>
 </head>
 
 <body>
---
+++ modules/errors/linkExpired.php
@@ -0,0 +1,23 @@
+ <?php
+    layoutUser('header');
+    ?>
+ <section class="mb-5">
+     <div class="container-fluid h-custom">
+         <div class="row d-flex flex-column justify-content-center align-items-center h-100">
+             <div class="col-md-9 col-lg-6 col-xl-5 d-flex justify-content-center align-items-center">
+                 <img src="<?php echo _HOST_URL_TEMPLATES; ?>/assets/images/TH.png"
+                     class="img-fluid" alt="Sample image">
+             </div>
+
+             <div class="col-md-8 col-lg-6 col-xl-4 d-flex flex-column align-items-center justify-content-center">
+                 <div class="d-flex flex-row align-items-center justify-content-center justify-content-lg-start text-center">
+                     <h2 class="fw-bold mb-5 me-3 text-danger">Link expired or does not exist</h2>
+                 </div>
+             </div>
+
+         </div>
+     </div>
+ </section>
+ <?php
+    layoutUser("footer");
+    ?>
