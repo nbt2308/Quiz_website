4bd061d4c7e6f7719bd7d9d6dc071bfbaf9562dc
Hoa Du <truchoa2019@gmail.com>
2025-11-07 19:42:08 +0700

    user layoutUser function to clear code

--- modules/home/index.php
+++ modules/home/index.php
@@ -4,45 +4,7 @@
 layoutUser('header');
 ?>
 <main>
-    <div class="category container">
-        <?php
-        $sql = "SELECT * FROM category LIMIT 8";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '
-                    <div class="category-title">
-                        <a href="?module=news_category&action=index&category_id=' . htmlspecialchars($row['category_id']) . '"><strong>' . htmlspecialchars($row['category_name']) . '</strong></a>
-                    </div>';
-            }
-
-            // More button
-            $sql1 = "SELECT * FROM category WHERE category_id>8";
-            $result1 = $conn->query($sql1);
-            if ($result1->num_rows > 0) {
-                while ($row1 = $result1->fetch_assoc()) {
-                    echo    '<div class="dropdown-center" style="z-index: 1;">
-                                <a class="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    More
-                                </a>
-
-                                <!-- <span class="visually-hidden">Toggle Dropdown</span> -->
-                                </button>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="?module=news_category&action=index&category_id=' . htmlspecialchars($row1['category_id']) . '">' . htmlspecialchars($row1['category_name']) . '</a></li>
-
-                                </ul>
-                            </div>';
-                }
-            }
-        } else {
-            echo '<div class="text-muted " style="font-size: 14px; text-align:center;">';
-            echo '<i class="bi bi-inbox fs-4 d-block mb-2 "></i>';
-            echo '<p>No data available</p>';
-            echo '</div>';
-        }
-        ?>
-    </div>
+    <?php layoutUser('categoryBar')?>
     <div class="main container mt-3">
         <div class="top-container ">
 
@@ -170,6 +132,14 @@ layoutUser('header');
                     </form>
                 </div>
             </div>
+
+            <!-- Back to top button -->
+            <button
+                type="button"
+                class="btn btn-primary btn-floating btn-lg"
+                id="btn-back-to-top">
+                <i class="fas fa-arrow-up"></i>
+            </button>
         </div>
     </div>
 </main>
--- modules/news_category/index.php
+++ modules/news_category/index.php
@@ -8,50 +8,7 @@ if (!empty($_GET['category_id'])) {
 // echo $category_id;
 ?>
 <main>
-    <div class="category container">
-        <?php
-        $sql = "SELECT * FROM category LIMIT 8";
-        $result = $conn->query($sql);
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-                echo '
-                    <div class="category-title">
-                        <a href="?module=news_category&action=index&category_id=' . htmlspecialchars($row['category_id']) . '"><strong>' . htmlspecialchars($row['category_name']) . '</strong></a>
-                    </div>';
-            }
-        ?>
-            <div class="dropdown-center" style="z-index: 1;">
-                <a class="btn btn-primary dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                    More
-                </a>
-
-                <!-- <span class="visually-hidden">Toggle Dropdown</span> -->
-                </button>
-                <ul class="dropdown-menu">
-                    <?php
-                    $sql = "SELECT * FROM category WHERE category_id>8";
-                    $result = $conn->query($sql);
-                    if ($result->num_rows > 0) {
-                        while ($row = $result->fetch_assoc()) {
-                            echo '<li><a class="dropdown-item" href="?module=news_category&action=index&category_id=' . htmlspecialchars($row['category_id']) . '">' . htmlspecialchars($row['category_name']) . '</a></li>';
-                        }
-                    }
-                    echo '<div class="text-muted " style="font-size: 14px; text-align:center;">';
-                    echo '<i class="bi bi-inbox fs-4 d-block mb-2 "></i>';
-                    echo '<p>No data available</p>';
-                    echo '</div>';
-                    ?>
-
-                </ul>
-            </div>
-        <?php
-        }
-        echo '<div class="text-muted " style="font-size: 14px; text-align:center;">';
-        echo '<i class="bi bi-inbox fs-4 d-block mb-2 "></i>';
-        echo '<p>No data available</p>';
-        echo '</div>';
-        ?>
-    </div>
+    <?php layoutUser('categoryBar')?>
     <div class="main-container container">
         <?php
         //in ten chu de
